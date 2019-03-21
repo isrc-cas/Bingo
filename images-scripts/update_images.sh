3ff6f6119e62a0721ecc9c286ddbc9f70cc3084f
taiyang <taiyangjobs@163.com>
2019-03-21 16:56:05 +0800

    update image updating script

--- images-scripts/update_images.sh
+++ images-scripts/update_images.sh
@@ -1,43 +1,54 @@
 #!/bin/bash
 
 function checkImage(){
-        update_results=`sudo docker pull $1`
-        if [[ $update_results =~ "Status: Downloaded newer image" ]]
-        then
-                echo "$1 : Downloaded newer image!"
-                return 1
+	update_results=`sudo docker pull $1`
+	if [[ $update_results =~ "Status: Downloaded newer image" ]]
+	then
+    		echo "$1 : Downloaded newer image!" >>  "$2.log"
+		return 1
 
-        elif [[ $update_results =~ "Status: Image is up to date" ]]
-        then
-                echo "$1 : Image is up to date!"
-                return 0
-        else
-                echo "$1 $update_results " >> 'log.txt'
-                return 2
-        fi
+	elif [[ $update_results =~ "Status: Image is up to date" ]]
+	then
+    		echo "$1 : Image is up to date!"
+		return 0
+	else
+		echo "$1 : Error $update_results" >> "$2.log"
+		return 2
+	fi
 }
+current=`date "+%Y-%m-%d-%H:%M:%S"`
+logname="image_updated_$current"
 update=false
-tmpId=""
+#tmpId=""
 tmpTag=""
 i=1
 while read line
 do
   array=(${line// / })
-  tag="${array[0]}:${array[1]}"
-  echo "$i: $tag"
-  let i++
-  if [[ $tag =~ "isrc.iscas.ac.cn" ]]
-  then
-      echo "$tag"
+  if [[ ${array[0]} =~ "isrc.iscas.ac.cn" ]]
+  then 
+     tag="${array[0]}-official:${array[1]}"
   else
-      update=false
-      checkImage $tag
-      result=$?
-      if [[ $result -eq 1 ]]
-      then
-                update=true
-                tmpTag=tag
-      fi
+     tag="${array[0]}:${array[1]}"
   fi
-  id=${array[2]}
-done < 'tags_list'
+  if [[ $tag =~ "isrc.iscas.ac.cn" ]] 
+  then 
+      	if [[ $update == true ]] 
+      	then
+     	 	echo `docker tag $tmpTag $tag`
+      		#echo `docker push $tmpTag $tag`
+        fi
+  else
+	echo "$i: $tag"
+        let i++
+      	update=false
+      	checkImage $tag $logname
+      	result=$?
+      	if [[ $result -eq 1 ]]
+      	then 
+         	update=true
+          	tmpTag=$tag 
+      	fi
+  fi
+  	#id=${array[2]}
+done < 'list-config'
